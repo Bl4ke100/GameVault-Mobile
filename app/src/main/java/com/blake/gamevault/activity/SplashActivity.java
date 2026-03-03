99a5779cbf36989e37e367a7fa195b73cdd2ae4b
Bl4ke100 <bl4ke100@gmail.com>
2026-03-03 15:54:42 +0530

    Added navbar

--- app/src/main/java/com/blake/gamevault/activity/SplashActivity.java
+++ app/src/main/java/com/blake/gamevault/activity/SplashActivity.java
@@ -1,9 +1,14 @@
 package com.blake.gamevault.activity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
 import android.view.WindowInsetsController;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +17,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.blake.gamevault.R;
+import com.squareup.picasso.Picasso;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -37,5 +43,31 @@ public class SplashActivity extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_splash);
+
+        ImageView imageView = findViewById(R.id.splashLogo);
+
+        Picasso.get()
+                .load(R.mipmap.gv_launcher_foreground)
+                .resize(300, 300)
+                .into(imageView);
+
+        new Handler(Looper.getMainLooper())
+                .postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        findViewById(R.id.splashProgressBar).setVisibility(View.VISIBLE);
+                    }
+                },1000);
+
+        new Handler(Looper.getMainLooper())
+                .postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        findViewById(R.id.splashProgressBar).setVisibility(View.INVISIBLE);
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },5000);
     }
 }
