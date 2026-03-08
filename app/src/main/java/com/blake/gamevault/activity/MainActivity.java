08f95789b1a9ceedfe2adc1ccf1d99f80b9c6961
Bl4ke100 <bl4ke100@gmail.com>
2026-03-08 08:53:16 +0530

    made the fragments

--- app/src/main/java/com/blake/gamevault/activity/MainActivity.java
+++ app/src/main/java/com/blake/gamevault/activity/MainActivity.java
@@ -1,26 +1,141 @@
 package com.blake.gamevault.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.blake.gamevault.R;
+import com.blake.gamevault.fragment.CartFragment;
+import com.blake.gamevault.fragment.HomeFragment;
+import com.blake.gamevault.fragment.LibraryFragment;
+import com.blake.gamevault.fragment.MessageFragment;
+import com.blake.gamevault.fragment.OrdersFragment;
+import com.blake.gamevault.fragment.ProfileFragment;
+import com.blake.gamevault.fragment.SettingsFragment;
+import com.blake.gamevault.fragment.ShopFragment;
+import com.blake.gamevault.fragment.WishlistFragment;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, NavigationBarView.OnItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private MaterialToolbar toolbar;
+    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
+
+    EditText searchTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//       EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.toolBar);
+        navigationView = findViewById(R.id.sideNav);
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else{
+                    finish();
+                }
+            }
         });
+
+        navigationView.setNavigationItemSelectedListener(this);
+        bottomNavigationView.setOnItemSelectedListener(this);
+
+        if (savedInstanceState == null){
+            loadFragment(new HomeFragment());
+        }
+
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.side_nav_profile){
+            loadFragment(new ProfileFragment());
+
+        }else if (itemId == R.id.side_nav_order){
+            loadFragment(new OrdersFragment());
+
+        }else if (itemId == R.id.side_nav_wishlist){
+            loadFragment(new WishlistFragment());
+
+        }else if (itemId == R.id.side_nav_Message){
+            loadFragment(new MessageFragment());
+
+        }else if (itemId == R.id.side_nav_settings){
+            loadFragment(new SettingsFragment());
+
+        }else if (itemId == R.id.side_nav_login){
+
+        }else if (itemId == R.id.side_nav_logout){
+
+        }else if (itemId == R.id.bottom_nav_home){
+            loadFragment(new HomeFragment());
+
+        }else if (itemId == R.id.bottom_nav_shop){
+            loadFragment(new ShopFragment());
+
+        }else if (itemId == R.id.bottom_nav_cart){
+            loadFragment(new CartFragment());
+
+        }else if (itemId == R.id.bottom_nav_library){
+            loadFragment(new LibraryFragment());
+
+
+        }
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+        return true;
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
