e5bcaa57786390921300a699195737530118082e
Bl4ke100 <bl4ke100@gmail.com>
2026-03-19 21:39:07 +0530

    made the login activity

---
+++ app/src/main/java/com/blake/gamevault/activity/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.blake.gamevault.activity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.SystemBarStyle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.blake.gamevault.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+    }
+}
